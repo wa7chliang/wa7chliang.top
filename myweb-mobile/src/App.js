20b281aa40e96c2973bd800115e5860ebd252a7a
wa7chliang <wa7chliang@163.com>
2019-02-23 00:53:14 +0800

    make mobile

--- myweb-mobile/src/App.js
+++ myweb-mobile/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom'
 import Index from './pages/index'
+import './normalize.scss'
 
 class App extends Component {
   render() {
--- myweb-mobile/src/pages/index/index.js
+++ myweb-mobile/src/pages/index/index.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import './index.scss'
 
 class Index extends Component {
   render() {
     return (
-      <div>
-        首页
+      <div className="index">
+        <div className="img-box">
+          <div className="img-border">
+            <div className="img-line"></div>
+          </div>
+          <div className='img'>
+            <img src='https://avatars3.githubusercontent.com/u/26890547?s=460&v=4' alt=""/>
+          </div>
+        </div>
       </div>
     );
   }
